272f6cc008b1a9b49963a42bef7b3a1d16ad6351
Yosiya Hinosawa <stibium121@gmail.com>
2013-09-06 12:41:05 +0900

    Add serialize test

--- www-source/test/domain-ipaddr-spec.js
+++ www-source/test/domain-ipaddr-spec.js
@@ -21,4 +21,24 @@ describe('IpRecord', function () {
         expect(ipRec.countryCode).toBe('JPN');
     });
 
+    it('is serializable as JSON format of object with 3 keys ipAddr, ctime and countryCode', function () {
+        var ipRec = new IpRecord({
+            ipAddr: '8.8.8.8',
+            ctime: 1300000000,
+            countryCode: 'JPN'
+        });
+
+        var json = ipRec.serialize();
+
+        expect(typeof json).toBe('string');
+
+        var object = JSON.parse(json);
+
+        expect(object).toEqual({
+            ipAddr: '8.8.8.8',
+            ctime: 1300000000,
+            countryCode: 'JPN'
+        });
+    });
+
 });
