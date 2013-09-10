dfab7a9c7e8ffe58a7cbe371e2b93b134b306ece
Yosiya Hinosawa <stibium121@gmail.com>
2013-09-10 19:09:49 +0200

    Add docs

--- www-source/jekyll-src/js/domain/iprecordfactory.js
+++ www-source/jekyll-src/js/domain/iprecordfactory.js
@@ -5,12 +5,21 @@ window.IpRecordFactory = (function ($, straw) {
 
     var exports = {};
 
+    /**
+     * (Async) create IpRecord object from a http request to checkip.dyndns.com/
+     * @return promise which will be done with IpRecord object or failed with error messages
+     */
     exports.createUsingDynDNS = function () {
         return straw.http.get('http://checkip.dyndns.com/').pipe(function (obj) {
             return new exports.createFromDynDNSResponseText(obj.content);
         });
     };
 
+    /**
+     * create IpRecord object from response text from http://checkip.dyndns.com/
+     * @param {string} text response text from http://checkip.dyndns.com/
+     * @return {IpRecord} IpRecord object representing current ip address
+     */
     exports.createFromDynDNSResponseText = function (text) {
         var obj = parseDynDNS(text);
 
@@ -20,6 +29,11 @@ window.IpRecordFactory = (function ($, straw) {
         });
     };
 
+    /**
+     * create IpRecord object from serialized JSON expression
+     * @param {string} str JSON expression of IpRecord
+     * @return {IpRecord} IpRecord represented by given JSON expression
+     */
     exports.createFromSerializedString = function (str) {
         var args;
 
