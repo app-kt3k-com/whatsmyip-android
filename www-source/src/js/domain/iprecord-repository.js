a69dd33f0ef291f4149db24f399413168f0b4877
Yosiya Hinosawa <stibium121@gmail.com>
2013-10-17 13:40:34 +0900

    Add iprecord-repository.js

---
+++ www-source/src/js/domain/iprecord-repository.js
@@ -0,0 +1,21 @@
+// Ip record repository class
+
+window.IpRecordRepository = (function ($, straw) {
+    'use strict';
+
+    var MAX_NUM_IP_RECORD = 256;
+
+    var exports = function () {
+    };
+
+    var prototype = exports.prototype;
+
+    prototype.getAll() {
+    }
+
+    prototype.add() {
+    }
+
+    return exports;
+
+}(window.$, window.straw));
