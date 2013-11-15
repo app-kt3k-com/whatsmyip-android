916e7be180f3a68a6fcd443e98622f7e3e85a8a7
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-16 01:16:36 +0900

    Move middleman structures

---
+++ config.rb
@@ -0,0 +1,12 @@
+# middleman config file
+
+set :source, 'src/main/middleman'
+
+set :build_dir, 'build/middleman'
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
