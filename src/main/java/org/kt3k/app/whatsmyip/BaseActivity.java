1072a371f456429a9e07665ff20916704bc8e6fc
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-17 01:21:10 +0900

    Refactor

--- src/main/java/org/kt3k/app/whatsmyip/BaseActivity.java
+++ src/main/java/org/kt3k/app/whatsmyip/BaseActivity.java
@@ -3,7 +3,7 @@ package com.kt3k.app.whatsmyip;
 import java.io.File;
 
 import org.kt3k.straw.Straw;
-import android.annotation.SuppressLint;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
@@ -29,90 +29,117 @@ public class BaseActivity extends Activity {
 	private String url = null;
 
 	/**
-	 * do window settings
+	 * set up window
 	 */
-	private void windowSetting() {
+	private void setUpWindow() {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 	}
 
 	/**
-	 * set up the main layout.
+	 * set up the main layout
 	 */
-	private void setUpContents() {
+	private void setUpLayout() {
 
-		// set up layout and add subviews.
-		layout = new LinearLayout(this);
-		layout.setOrientation(LinearLayout.VERTICAL);
-		setContentView(layout);
+		// set up linear layout
+		this.layout = new LinearLayout(this);
+
+        // enable only portrait orientation
+		this.layout.setOrientation(LinearLayout.VERTICAL);
+
+        // set layout as content view
+		this.setContentView(layout);
 
 		// set up and layout the WebView
-		setUpWebView();
-		layout.addView(webView, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f));
+		this.setUpWebView();
+		this.layout.addView(this.webView, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f));
 
-		// if the ad-flag is true, then set up and layout the AdView.
-		if (adEnabled) {
-			setUpAdView();
+		// if ad enable then add ad
+		if (this.adEnabled) {
+			this.setUpAdView();
 
-			adView.setGravity(Gravity.BOTTOM);
+			this.adView.setGravity(Gravity.BOTTOM);
 
 			LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.0f);
 
 			p.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
 
-			layout.addView(adView, p);
+			this.layout.addView(this.adView, p);
 		}
 	}
 
 	/**
-	 * do WebView settings
+	 * set up the WebView
 	 */
-	@SuppressLint("SetJavaScriptEnabled")
 	private void setUpWebView() {
+
+        // create WebView
 		webView = new WebView(this);
+
+        //
 		if(url == null || "".equals(url)) {
-			url = this.getAppHome() + this.getAppIndex();
+			url = this.getAppHome();
 		}
 
-		// スクロールバーのスタイルを設定
+		// setting scroll bar styles
 		webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 		webView.setScrollbarFadingEnabled(true);
 
-		// WebViewClient と WebChromeClient をセット
-		webView.setWebViewClient(new CustomWebViewClient());
+		// set WebViewClient
+		webView.setWebViewClient(new CustomWebViewClient(this.getString("wait_message", "Loading")));
+
+        // set WebChromeClient
 		webView.setWebChromeClient(new WebChromeClient());
 
-		// JavaScript を有効化
+		// enable JavaScript
 		webView.getSettings().setJavaScriptEnabled(true);
-		// DomStorage (Web Storage) を有効化
+
+		// enable DomStorage (i.e. localStorage and SessionStorage)
 		webView.getSettings().setDomStorageEnabled(true);
-		// DB の path を指定する。
+
+		// set DB path
 		webView.getSettings().setDatabasePath((new File(getCacheDir(), "/database")).toString());
-		// 背景色
+
+		// set background color white
 		webView.setBackgroundColor(Color.WHITE);
 
-		// Straw insert
+		// insert Straw into webView and init basic plugins
 		Straw.insertInto(webView).addPlugins(org.kt3k.straw.plugin.BasicPlugins.names);
 
-
+        // load url
 		webView.loadUrl(url);
 	}
 
+    /**
+     * set up the AdView
+     */
 	private void setUpAdView() {
-		adView = new AdView(this, AdSize.BANNER, getPublisherId());
+		adView = new AdView(this, AdSize.BANNER, this.getPublisherId());
 
+        // set up the request
 		AdRequest adRequest = new AdRequest();
+
+        // set test devices
 		adRequest.addTestDevice(AdRequest.TEST_EMULATOR);
 		adRequest.addTestDevice("DA5B0069BA8827B46FD8DBDB70EB7FAE"); // Test Device 1
 		adRequest.addTestDevice("7DCEAF5D75884209E5102213D1EA33C6"); // Test Device 2
+
+        // request an ad
 		adView.loadAd(adRequest);
 	}
 
-	private String getStringWithDefault(String key, String defaultValue) {
-		String str = null;
+    /**
+     * get string for key from string resources
+     * @param key to get
+     * @param defaultValue if value unavailable
+     * @return string for key or default if string unavailable
+     */
+	private String getString(String key, String defaultValue) {
+		String str;
 
 		try {
 			int resId = R.string.class.getField(key).getInt(null);
 			str = this.getString(resId);
+
 		} catch (Exception e) {
 			str = defaultValue;
 		}
@@ -121,50 +148,45 @@ public class BaseActivity extends Activity {
 	}
 
 	private String getAppHome() {
-		return this.getStringWithDefault("app_home", "file:///android_asset/");
-	}
-
-	private String getAppIndex() {
-		return this.getStringWithDefault("app_index", "index.html");
+		return this.getString("app_home", "file:///android_asset/") + this.getString("app_index", "index.html");
 	}
 
 	private String getPublisherId() {
-		return this.getStringWithDefault("app_publisher_id", "");
+		return this.getString("app_publisher_id", "");
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-        this.windowSetting();
+        this.setUpWindow();
 
-        this.setUpContents();
+        this.setUpLayout();
 	}
 
     class CustomWebViewClient extends WebViewClient {
         private ProgressDialog dialog;
+        private String loadingMessage;
 
-        public CustomWebViewClient() {
+        public CustomWebViewClient(String loadingMessage) {
             super();
-            dialog = null;
+            this.dialog = null;
+            this.loadingMessage = loadingMessage;
         }
 
-        //ページ読み込み開始時の動作
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            //ダイアログを作成して表示
-            dialog = new ProgressDialog(view.getContext());
-            dialog.setMessage(getStringWithDefault("wait_message", "Loading"));
-            dialog.show();
+            this.dialog = new ProgressDialog(view.getContext());
+            this.dialog.setMessage(this.loadingMessage);
+            this.dialog.show();
         }
 
-        //ページ読み込み終了時の動作
         @Override
         public void onPageFinished(WebView view, String url) {
             if (null != dialog) {
                 //ダイアログを削除
-                dialog.dismiss();
-                dialog = null;
+                this.dialog.dismiss();
+                this.dialog = null;
             }
         }
     }
