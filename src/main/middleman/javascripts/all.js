bda5f8dd502ac7fdf0fe9fd18387bcebeca21d32
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-18 00:30:37 +0900

    Move i18n.js to its repository

--- src/main/middleman/javascripts/all.js
+++ src/main/middleman/javascripts/all.js
@@ -3,4 +3,6 @@
 //= require straw-android.js/src/straw-android.js
 //= require_tree ../../../../bower_components/straw-android-plugin.js/src
 
+//= require 'i18n.js/i18n.js'
+
 //= require_tree ../../js
