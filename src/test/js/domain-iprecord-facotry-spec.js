4377cc0da3748f634b429a843b62492673bf28dc
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-28 22:52:14 +0900

    Add createUsingDynDNS test

--- src/test/js/domain-iprecord-facotry-spec.js
+++ src/test/js/domain-iprecord-facotry-spec.js
@@ -3,6 +3,10 @@ var describe = window.describe;
 var expect = window.expect;
 var it = window.it;
 
+var sinon = window.sinon;
+var straw = window.straw;
+var $ = window.$;
+
 describe('IpRecordFactory', function () {
     'use strict';
 
@@ -27,6 +31,31 @@ describe('IpRecordFactory', function () {
             expect(ipRec.countryCode).toEqual(null);
         });
 
+    });
+
+
+    describe('createUsingDynDNS', function () {
+
+        it('create IpRecord using DynDNS api', function () {
+
+            var stubGet = sinon.stub(straw.http, 'get');
+            stubGet.withArgs('http://checkip.dyndns.com/').returns($.Deferred().resolve({content: '<html><body>Ip Address is: 11.12.13.14</body></html>'}));
+
+            IpRecordFactory.createUsingDynDNS().done(function (ipRecord) {
+
+                expect(ipRecord instanceof IpRecord).toBe(true);
+                expect(ipRecord.ipAddr).toBe('11.12.13.14');
+
+            });
+
+            stubGet.restore();
+
+        });
+
+    });
+
+
+    describe('createFromSerializedString', function () {
     });
 
 });
