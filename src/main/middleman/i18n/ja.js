8ccd3e6fd89e7be8fa3feec4d6c756a18dce3148
Yoshiya Hinosawa <stibium121@gmail.com>
2014-01-02 15:26:24 +0900

    Change thank you message of 'ja'

--- src/main/middleman/i18n/ja.js
+++ src/main/middleman/i18n/ja.js
@@ -3,7 +3,7 @@ window.i18n.setResource({
     "common.brand.address": "Address",
     "common.dialog.ok": "はい",
     "common.dialog.not_now": "あとで",
-    "common.dialog.thanks_for_using_app": "ご利用あざす！",
+    "common.dialog.thanks_for_using_app": "使ってくれてありがとう！",
     "common.dialog.would_you_review_the_app": "アプリを評価しませんか？",
     "common.loading": "読み込み中...",
     "common.googleplay": "Google Play",
