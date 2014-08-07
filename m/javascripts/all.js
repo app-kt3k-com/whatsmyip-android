ffab1d57461105a17fc63fe969aa6693060a2093
Yoshiya Hinosawa <stibium121@gmail.com>
2014-08-07 17:05:38 +0100

    Resolve straw-android dependency through registry

--- m/javascripts/all.js
+++ m/javascripts/all.js
@@ -6,9 +6,10 @@
 
 //= require es6-promise
 
-//= require straw-android.js/src/straw-android.js
-//= require_tree ../../bower_components/straw-android-plugin.js/src
-
 //= require t10
+
+//= require straw-android
+
+//= require_tree ../../bower_components/straw-android-plugin.js/src
 
 //= require_tree ../../app/src
