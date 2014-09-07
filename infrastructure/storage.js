47b991dc60b4b89868cdae3b7ca7138f2cd1ac61
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-07 13:05:30 +0100

    Add infrastructures

---
+++ infrastructure/storage.js
@@ -0,0 +1,22 @@
+
+window.infrastructure = window.infrastructure || {};
+
+window.infrastructure.storage = (function (straw, Promise) {
+    'use strict';
+
+    var exports = {};
+
+    straw = Promise;
+    Promise = straw;
+
+    exports.set = function (key, value) {
+        return key && value;
+    };
+
+    exports.get = function (key, defaultValue) {
+        return key && defaultValue;
+    };
+
+    return exports;
+
+}(window.straw, window.Promise));
