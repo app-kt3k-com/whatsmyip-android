dd0e0d0436e53ef02003fc5ec8898795236d215a
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-29 19:10:56 +0100

    Always open this app's market page

--- infrastructure/external-service.js
+++ infrastructure/external-service.js
@@ -4,13 +4,15 @@ window.infrastructure = window.infrastructure || {};
 window.infrastructure.externalService = (function (straw) {
     'use strict';
 
+    var APP_ID = 'com.kt3k.app.whatsmyip';
+
     var exports = {};
 
-    exports.openMarketLink = function (id) {
+    exports.openMarketLink = function () {
 
-        straw.uri.open('market://details?id=' + id).fail(function () {
+        straw.uri.open('market://details?id=' + APP_ID).then(null, function () {
 
-            straw.browser.open('https://play.google.com/store/apps/id=' + id);
+            straw.browser.open('https://play.google.com/store/apps/id=' + APP_ID);
 
         });
 
